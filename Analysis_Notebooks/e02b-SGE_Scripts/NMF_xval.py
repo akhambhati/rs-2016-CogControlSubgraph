78efb906e38ee132a3a596728c0856d6adc88217
Ankit Khambhati <akhambhati@gmail.com>
2017-04-13 11:01:32 -0400

    Added SGE helper script for running NMF cross-validation

---
+++ Analysis_Notebooks/e02b-SGE_Scripts/NMF_xval.py
@@ -0,0 +1,41 @@
+#!/bin/zsh
+
+source activate echobase
+ipython -c "
+
+######
+from __future__ import division
+
+import os
+os.environ['MKL_NUM_THREADS'] = '1'
+os.environ['NUMEXPR_NUM_THREADS'] = '1'
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+import sys
+import glob
+import numpy as np
+
+sys.path.append('$1')
+import Echobase
+Subgraph = Echobase.Network.Partitioning.Subgraph
+
+path_ExpData = '$2'
+
+# Load the configuration matrix and parameter list
+cfg_data = np.load('{}/NMF_Optimization.CfgMatr.npz'.format(path_ExpData))
+cfg_matr = cfg_data['cfg_matr']
+param_list = np.load('{}/NMF_CrossValidation.Param_List.npz'.format(path_ExpData))['param_list']
+
+# Grab the task ID of the current job (and the associated parameter dictionary)
+sge_task_id = int(os.environ['SGE_TASK_ID'])-1
+param_dict = param_list[sge_task_id]
+param_dict['train_ix'] = map(int, param_dict['train_ix'])
+param_dict['test_ix'] = map(int, param_dict['test_ix'])
+
+qmeas_dict = Subgraph.optimize_nmf.run_xval_paramset(cfg_matr, param_dict)
+
+np.savez('{}/NMF_CrossValidation.Param.{}.npz'.format(path_ExpData, sge_task_id),
+         qmeas_dict=qmeas_dict)
+######
+
+"
